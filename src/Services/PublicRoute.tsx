ac267a4a7e5e0ed91a75395e9a7ca69a13b42cf2
Saksham Saini <sakshamsaini654@gmail.com>
2025-03-19 15:59:05 +0530

    Update PublicRoute.tsx

--- src/Services/PublicRoute.tsx
+++ src/Services/PublicRoute.tsx
@@ -1,18 +1,23 @@
 import { jwtDecode } from "jwt-decode";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 interface PublicRouteProps {
   children: JSX.Element;
 }
 const PublicRoute: React.FC<PublicRouteProps> = ({ children }) => {
   const token = useSelector((state: any) => state.jwt);
-  if (token) {
+  const location = useLocation();
+
+  // Allow access to both /login and /signup for unauthenticated users
+  if (!token) {
+    return children;
+  }
+
+  // If user is authenticated, prevent access to login/signup and redirect to home
+  if (location.pathname === "/login" || location.pathname === "/signup") {
     return <Navigate to="/" />;
   }
-  // const decoded: any = jwtDecode(token);
-  // if (allowedRoles && !allowedRoles.includes(decoded.applicationType)) {
-  //   return <Navigate to="/unauthorized" />;
-  // }
+
   return children;
 };
 
