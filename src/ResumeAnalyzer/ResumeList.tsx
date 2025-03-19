47d14da5fd7612e5fb8a2fc861a3480602e2cfce
Saksham Saini <sakshamsaini654@gmail.com>
2025-03-19 18:18:03 +0530

    Update ResumeList.tsx

--- src/ResumeAnalyzer/ResumeList.tsx
+++ src/ResumeAnalyzer/ResumeList.tsx
@@ -1,7 +1,7 @@
 const ResumeList = () => {
   return (
     <div className="p-18 m-4">
-      <h2 className="text-mine-shaft-300 text-2xl">Prepare for Interview</h2>
+{/*       <h2 className="text-mine-shaft-300 text-2xl">Prepare for Interview</h2> */}
     </div>
   );
 };
